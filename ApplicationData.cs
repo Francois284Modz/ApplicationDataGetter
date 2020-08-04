a142afeb2b873a155562c1f7b2b270072532f48f
francois emile <ti-peewee@hotmail.fr>
2020-08-04 02:56:10 +0200

    Ajoutez des fichiers projet.

---
+++ ApplicationData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace ApplicationDataGetter
+{
+    public class ApplicationData
+    {
+        /// <summary>
+        /// Returns an ApplicationData for the packageid from google play
+        /// </summary>
+        /// <param name="packageid"></param>
+        public ApplicationData(string packageid)
+        {
+            WebClient client = new WebClient();
+            string downloadedString = client.DownloadString("https://play.google.com/store/apps/details?id=" + packageid);
+
+            /*  RegexOptions options = RegexOptions.Multiline;
+
+              foreach (Match m in Regex.Matches(downloadedString, Strings.LogoPattern, options))
+              {
+                  this.IconURL = m.Groups[1].Value;
+              }
+              */
+            this.IconURL = Strings.GetLogo(downloadedString);
+            this.Name = Strings.GetAPKName(downloadedString);
+            this.Version = Strings.GetAPKVersion(downloadedString);
+            this.Updated = Strings.GetLastUpdate(downloadedString);
+            this.PackageID = packageid;
+            this.Size = Strings.GetAPKSize(downloadedString);
+        }
+
+       
+
+        public string PackageID { get; internal set; }
+        public string Name { get; internal set; }
+        public string IconURL { get; internal set; }
+        // public Version Version { get; internal set; }
+        public Version Version { get; internal set; }
+        public DateTime Updated { get; internal set; }
+        public string Size { get; internal set; }
+    }
+}
---
+++ Strings.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ApplicationDataGetter
+{
+    internal static class Strings
+    {
+        private static string LogoPattern = @"<div class=""xSyT2c""><img src=""(.*?)""";
+        private static string VersionPattern = @"Current Version<\/div><span class=""htlgb""><div class=""IQ1z0d""><span class=""htlgb"">(.*?)<\/span><\/div>";
+        private static string NamePattern = @"<title id=""main-title"">(.*)<\/title>";
+        private static string UpdatePattern = @"Updated<\/div><span class=""htlgb""><div class=""IQ1z0d""><span class=""htlgb"">(.*?)<\/span>";
+        private static string SizePattern = @"<div class=""BgcNfc"">Size<\/div><span class=""htlgb""><div class=""IQ1z0d""><span class=""htlgb"">(.*?)<\/span><\/div><\/span><\/div>";
+        /// <summary>
+        /// Return APK Logo Url
+        /// </summary>
+        /// <param name="downloadedString"></param>
+        /// <returns></returns>
+        public static string GetLogo(string str)
+        {
+            RegexOptions options = RegexOptions.Multiline ;
+            string value = null;
+            foreach (Match m in Regex.Matches(str, Strings.LogoPattern, options))
+            {
+
+                value = m.Groups[1].Value;
+            }
+            return value;
+        }
+        /// <summary>
+        /// Return APK Name
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string GetAPKName(string str)
+        {
+            RegexOptions options = RegexOptions.Multiline ;
+            string value1 = null;
+            foreach (Match m in Regex.Matches(str, Strings.NamePattern, options))
+            {
+             var strresult = m.Groups[1].Value.Replace("- Apps on Google Play", "");
+                value1 = strresult;
+            }
+            return value1;
+        }
+        /// <summary>
+        /// Return APK Version
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static Version GetAPKVersion(string str)
+        {
+            RegexOptions options = RegexOptions.Multiline ;
+            string value1 = null;
+            foreach (Match m in Regex.Matches(str, VersionPattern, options))
+            {
+                 value1 = m.Groups[1].Value;
+            }
+            if (Version.TryParse(value1, out Version result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new Exception("Failed to parse Value");
+            }
+        }
+
+        /// <summary>
+        /// Return APK Update Version
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public  static DateTime GetLastUpdate(string str)
+        {
+            RegexOptions options = RegexOptions.Multiline;
+            string value1 = null;
+            foreach (Match m in Regex.Matches(str, UpdatePattern, options))
+            {
+                value1 = m.Groups[1].Value;
+            }
+            if (DateTime.TryParse(value1, out DateTime result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new Exception("Failed to parse Value");
+            }
+        }
+        /// <summary>
+        /// Return APK Size
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string GetAPKSize(string str)
+        {
+            RegexOptions options = RegexOptions.Multiline;
+            string value1 = null;
+            foreach (Match m in Regex.Matches(str, Strings.SizePattern, options))
+            {
+                 value1 = m.Groups[1].Value;
+               
+            }
+            return value1;
+        }
+    }
+}
